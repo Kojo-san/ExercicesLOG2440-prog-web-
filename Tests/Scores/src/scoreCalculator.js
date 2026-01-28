063b6924bd3943e894900647fdba4b35faf79942
Gamaliel <gamaliel.kalefe@etud.polymtl.ca>
2026-01-28 15:22:53 -0500

    Implement ScoreCalculator class with methods for calculating average scores from reviews

--- Tests/Scores/src/scoreCalculator.js
+++ Tests/Scores/src/scoreCalculator.js
@@ -1,4 +1,33 @@
 export default class ScoreCalculator {
     constructor() { }
+    getStats(reviews) {
+        const finalGrades = reviews.reduce((acc, review) => {
+            if (this.validateStars(review)) {
+                if (review.author) {
+                    acc.authoredAverage.push(review.stars);
+                } else {
+                    acc.anonymousAverage.push(review.stars);
+                }
+                acc.totalAverage.push(review.stars);
+            }
+            return acc;
+        }, {
+            totalAverage: [],
+            authoredAverage: [],
+            anonymousAverage: []
+        }); 
+        return {
+            totalAverage: this.calculateAverages(finalGrades.totalAverage),
+            anonymousAverage: this.calculateAverages(finalGrades.anonymousAverage),
+            authoredAverage: this.calculateAverages(finalGrades.authoredAverage)
+        };
+    }
 
+    validateStars(review) {
+        return review.stars >=1 && review.stars <= 5;
+    }
+
+    calculateAverages(array) {
+        return array.reduce((acc, x) => acc + x, 0) /array.length;
+    }
 }
